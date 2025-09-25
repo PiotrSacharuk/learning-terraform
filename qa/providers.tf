bedaf24dab32db799913288bcbbd6ca2d17f4894
PiotrSacharuk <piotr.sacharuk@gmail.com>
2025-09-25 12:37:21 +0200

    Add providers info

--- qa/providers.tf
+++ qa/providers.tf
@@ -1,6 +1,14 @@
 terraform {
   required_version = ">= 1.13.0"
 
+  cloud {
+    organization = "piotrsacharuk"
+
+    workspaces {
+      name = "learning-terraform-qa"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
